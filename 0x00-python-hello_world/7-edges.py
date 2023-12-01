197528f5b8efe1b8c011297a9246668d0c263e4c
DarkWeirdo <sonic_unleashed@live.com>
2023-12-01 14:24:30 +0200

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word_first_3[:3]}")
+print(f"Last 2 letters: {word_last_2[-2:]}")
+print(f"Middle word: {middle_word[1:len(word)-1]}")
