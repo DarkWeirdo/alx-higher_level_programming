8dd3a1979366194490391569ce5683a363b319f5
DarkWeirdo <sonic_unleashed@live.com>
2023-12-09 17:09:26 +0200

    t6 fix1: fix repeated print of last number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for i in range(0, 10):
     for j in range(i+1, 10):
         if(i < j):
-            print("{:01d}{:01d}".format(i,j), end=", ")
             if(i==8) and (j==9):
                 print("{:01d}{:01d}\n".format(i,j), end="")
+            else:
+                print("{:01d}{:01d}".format(i,j), end=", ")
