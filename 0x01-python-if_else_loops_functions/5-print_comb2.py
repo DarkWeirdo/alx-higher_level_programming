ef7e953033499b31aaa4f3648ec4cfb34e0e061f
DarkWeirdo <sonic_unleashed@live.com>
2023-12-09 16:14:05 +0200

    t5 fix 1: condition for 99 before normal print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for number in range(100):
-    print("{:02d}".format(number), end=", ")
     if number == 99:
         print("{:02d}\n".format(number), end="")
+    else:
+        print("{:02d}".format(number), end=", ")
+    
