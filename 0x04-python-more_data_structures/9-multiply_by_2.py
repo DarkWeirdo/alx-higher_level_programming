1f2010651338ccd4c7ef99c87a91d10bead01df4
DarkWeirdo <sonic_unleashed@live.com>
2023-12-27 03:22:58 +0200

    tasks 9 to 11

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """
+    returns a new dictionary with all values multiplied by 2
+    You can assume that all values are only integers
+    Returns a new dictionary
+    """
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
+
+    for i in list_keys:
+        new_dir[i] *= 2
+
+    return new_dir
