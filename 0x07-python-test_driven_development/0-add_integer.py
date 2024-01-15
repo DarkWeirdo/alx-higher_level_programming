d25cd952df3483132255893a475e7782081544a2
DarkWeirdo <sonic_unleashed@live.com>
2024-01-15 20:59:47 +0200

    task 0 test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+def add_integer(a, b=98):
+    """
+    Adds two numbers after casting them to integers.
+
+    :param a: The first number, must be an integer or float.
+    :param b: The second number, defaults to 98 if not provided. Must be an integer or float.
+    :return: The addition of a and b casted to integers.
+    :raises TypeError: If a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)) or a is None or isinstance(a, bool):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)) or b is None or isinstance(b, bool):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
