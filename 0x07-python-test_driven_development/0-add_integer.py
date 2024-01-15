bc5aa6883fe34606c3ebfacbd2df4365fcb78631
DarkWeirdo <sonic_unleashed@live.com>
2024-01-15 21:01:54 +0200

    change in doc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@ def add_integer(a, b=98):
     Adds two numbers after casting them to integers.
 
     :param a: The first number, must be an integer or float.
-    :param b: The second number, defaults to 98 if not provided. Must be an integer or float.
+    :param b: The second number, defaults to 98 if not provided.
     :return: The addition of a and b casted to integers.
     :raises TypeError: If a or b is not an integer or float.
     """
