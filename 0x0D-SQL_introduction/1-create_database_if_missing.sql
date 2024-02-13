7230c97ed88921b3004e66b39a38241c91b8bab7
DarkWeirdo <sonic_unleashed@live.com>
2024-02-13 21:44:59 +0200

    task 1 revert

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,5 @@
 -- This script creates the database hbtn_0c_0 if it does not already exist.
 -- Task: Create a database named hbtn_0c_0 if it does not exist.
 
-IF NOT EXISTS (SELECT 1
-FROM sys.databases
-WHERE name = 'hbtn_0c_0')
-    CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE
+IF NOT EXISTS hbtn_0c_0;
