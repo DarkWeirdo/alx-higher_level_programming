6be308f56d7535a94adc2d507e153d750893c5d9
DarkWeirdo <sonic_unleashed@live.com>
2024-02-14 21:48:09 +0200

    task 8

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+-- Task: Display the number of records with id =  89 in the table first_table
+-- This script counts the number of records in the first_table where the id equals  89.
+-- The database name is passed as an argument to the mysql command.
+
+SELECT COUNT(*)
+FROM FIRST_TABLE
+WHERE ID =  89;
