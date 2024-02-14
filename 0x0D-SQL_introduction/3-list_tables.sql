d07df1ab99297e3846c76990b8d978c37d461b7b
DarkWeirdo <sonic_unleashed@live.com>
2024-02-14 19:31:13 +0200

    tasks 3 and 4 fixes

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,4 @@
 -- This script lists all the tables in the specified database.
 -- Task: List all tables in the current database.
-
-USE your_database_name;
+USE mysql;
 SHOW TABLES;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,10 +1,7 @@
 -- This script creates the table first_table if it does not already exist.
 -- Task: Create a table named first_table with columns id and name if it does not exist.
 
-CREATE TABLE
-IF NOT EXISTS first_table
-(
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
-    name VARCHAR
-(256)
+    name VARCHAR(256)
 );
