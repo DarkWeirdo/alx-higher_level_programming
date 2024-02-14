7eca16908e63790493b018ed1ec76a3be7ec6a9f
DarkWeirdo <sonic_unleashed@live.com>
2024-02-14 19:48:49 +0200

    task 5 fix

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,6 @@
 -- This script prints the full description of the table first_table.
 -- Task: Print the full description of the table first_table.
-
-SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (\\n', GROUP_CONCAT(COLUMN_TYPE), '\\n) ENGINE=', ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION, ';') AS Create_Table
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (\\n', GROUP_CONCAT(CONCAT('  `', COLUMN_NAME, '` ', COLUMN_TYPE)), '\\n) ENGINE=', ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION, ' COLLATE=', TABLE_COLLATION, ';') AS Create_Table
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
 
