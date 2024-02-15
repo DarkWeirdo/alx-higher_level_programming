4f1c7f065cc36ba4c22e7ea70d5dc4670bace4a9
DarkWeirdo <sonic_unleashed@live.com>
2024-02-15 11:42:38 +0200

    TASK 5 FIX

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- This script prints the full description of the table first_table.
 -- Task: Print the full description of the table first_table.
-SHOW CREATE TABLE [IF NOT EXISTS] first_table;
+SHOW
+CREATE TABLE
+    first_table;
