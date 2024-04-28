2266fc0d60efd08d303ed31ebeac26044fa43f4e
DarkWeirdo <sonic_unleashed@live.com>
2024-04-29 02:28:33 +0300

    task 9 fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,9 +3,13 @@
 
 function add (a, b) {
   if (isNaN(a) || isNaN(b)) {
-    console.log('Error: One or both arguments are not numbers.');
+    console.log('NaN');
   } else {
     const sum = Number(a) + Number(b);
     console.log(sum);
   }
 }
+
+// Example usage from command line arguments
+const args = process.argv.slice(2); // Skip the first two elements
+add(args[0], args[1]);
