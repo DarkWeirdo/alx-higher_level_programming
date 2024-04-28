cf7acdc48d82b125af8b9f0705415227a1258966
DarkWeirdo <sonic_unleashed@live.com>
2024-04-29 00:33:08 +0300

    task 6 fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let message of messages) {
-    console.log(message);
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const message of messages) {
+  console.log(message);
 }
