5b98555293792ec6fefecfcb2005eb0f9a6ddaac
DarkWeirdo <sonic_unleashed@live.com>
2024-05-23 04:27:54 +0300

    task 5 fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    fs.writeFileSync(filePath, body, 'utf-8');
-    console.log(`File saved successfully at ${filePath}`);
-  } else {
-    console.error(`Error: ${error}`);
-  }
+    if (!error && response.statusCode === 200) {
+        fs.writeFile(filePath, body, 'utf8', (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    } else {
+        console.error(`Error fetching webpage: ${error}`);
+    }
 });
