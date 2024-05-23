ccef83ad05df3c5f33f364597e8d57f26ef6878a
DarkWeirdo <sonic_unleashed@live.com>
2024-05-23 04:28:40 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,13 +7,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        fs.writeFile(filePath, body, 'utf8', (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    } else {
-        console.error(`Error fetching webpage: ${error}`);
-    }
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  } else {
+    console.error(`Error fetching webpage: ${error}`);
+  }
 });
