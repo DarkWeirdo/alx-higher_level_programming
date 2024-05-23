87b87d70e850e5a3ee4eee0e97652afa6aabb3fc
DarkWeirdo <sonic_unleashed@live.com>
2024-05-23 03:54:14 +0300

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+let count = 0;
+
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+
+    films.forEach(film => {
+      const characters = film.characters;
+      if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  } else {
+    console.log('Error fetching data from the API');
+  }
+});
